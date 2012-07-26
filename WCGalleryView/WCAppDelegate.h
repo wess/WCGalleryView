923534924662f0777175a6f3afddfccedd3fb45c
Wess Cope <wcope@me.com>
2012-07-25 20:46:30 -0400

    Initial Commit

---
+++ WCGalleryView/WCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCAppDelegate.h
+//  WCGalleryView
+//
+//  Created by Wess Cope on 7/25/12.
+//  Copyright (c) 2012 Wess Cope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
